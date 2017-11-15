436b5b56eb7542866ff03b1cd6bed16963fb8183
yan <18300602795@163.com>
2017-11-15 10:11:07 +0800

    完善了分类页面

---
+++ app/src/main/java/com/i76game/activity/EarnActivity.java
@@ -0,0 +1,17 @@
+package com.i76game.activity;
+
+/**
+ * Created by Administrator on 2017/11/15.
+ */
+
+public class EarnActivity extends BaseActivity {
+    @Override
+    protected int setLayoutResID() {
+        return 0;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
